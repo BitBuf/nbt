55647741b92d552dd6212eb4c3eb6089100dd9d1
Dewy <dewy@dewy.dev>
2021-08-20 23:13:30 +0100

    feat: initial float and double tag impls

---
+++ src/main/java/dev/dewy/nbt/tags/primitive/DoubleTag.java
@@ -0,0 +1,46 @@
+package dev.dewy.nbt.tags.primitive;
+
+import dev.dewy.nbt.TagRegistry;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoubleTag extends NumericalTag<Double> {
+    private double value;
+
+    public DoubleTag(String name, double value) {
+        this.setName(name);
+        this.setValue(value);
+    }
+
+    @Override
+    public byte getId() {
+        return 6;
+    }
+
+    @Override
+    public Double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public void write(DataOutput output, int depth, TagRegistry registry) throws IOException {
+        output.writeDouble(this.value);
+    }
+
+    @Override
+    public DoubleTag read(DataInput input, int depth, TagRegistry registry) throws IOException {
+        this.value = input.readDouble();
+
+        return this;
+    }
+}
---
+++ src/main/java/dev/dewy/nbt/tags/primitive/FloatTag.java
@@ -0,0 +1,46 @@
+package dev.dewy.nbt.tags.primitive;
+
+import dev.dewy.nbt.TagRegistry;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class FloatTag extends NumericalTag<Float> {
+    private float value;
+
+    public FloatTag(String name, float value) {
+        this.setName(name);
+        this.setValue(value);
+    }
+
+    @Override
+    public byte getId() {
+        return 5;
+    }
+
+    @Override
+    public Float getValue() {
+        return this.value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    @Override
+    public void write(DataOutput output, int depth, TagRegistry registry) throws IOException {
+        output.writeFloat(this.value);
+    }
+
+    @Override
+    public FloatTag read(DataInput input, int depth, TagRegistry registry) throws IOException {
+        this.value = input.readFloat();
+
+        return this;
+    }
+}
