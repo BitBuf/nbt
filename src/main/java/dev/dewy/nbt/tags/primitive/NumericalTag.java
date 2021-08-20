b7b7ad90810e752c7f8994c07fbc223fa2dbff13
Dewy <dewy@dewy.dev>
2021-08-20 23:04:42 +0100

    feat: abstract numerical tag

---
+++ src/main/java/dev/dewy/nbt/tags/primitive/NumericalTag.java
@@ -0,0 +1,32 @@
+package dev.dewy.nbt.tags.primitive;
+
+import dev.dewy.nbt.Tag;
+
+public abstract class NumericalTag<T extends Number> extends Tag {
+    @Override
+    public abstract T getValue();
+
+    public byte byteValue() {
+        return this.getValue().byteValue();
+    }
+
+    public short shortValue() {
+        return this.getValue().shortValue();
+    }
+
+    public int intValue() {
+        return this.getValue().intValue();
+    }
+
+    public long longValue() {
+        return this.getValue().longValue();
+    }
+
+    public float floatValue() {
+        return this.getValue().floatValue();
+    }
+
+    public double doubleValue() {
+        return this.getValue().doubleValue();
+    }
+}
